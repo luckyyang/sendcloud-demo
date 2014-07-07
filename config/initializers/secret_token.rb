b61451381cf76128bd8fdd3dbe9be6c4ab4ce276
Liu Jiyang <2411mail@gmail.com>
2014-07-07 09:36:48 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendcloudDemo::Application.config.secret_token = '2c67b93a5fa3945d50a8b58c603f5ff6ef31e0090132e2f0cbc4378865169f97d4a1fd0fdaf0718a7eadd8097549d88774bb3e3f5039aa284c7750464d3def4e'
