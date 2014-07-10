8e2860274c97e9e773e89fd1bb6f3a638389f2b1
Liu Jiyang <2411mail@gmail.com>
2014-07-10 19:22:26 +0800

    add user name display

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
   default :from => "no-reply@maodou.io"
 
   def registration_confirmation(user)
+    @user = user
     mail(:to => user.email, :subject => "欢迎来到毛豆网")
   end
 end
